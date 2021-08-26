a4b92b2c966eff6113d3df071f1623509b3a2781
Kristian Broggi <k.broggi@icloud.com>
2021-08-26 07:33:52 -0400

    Adding a test suite and code coverage

--- recipe.js
+++ recipe.js
@@ -1,14 +1,13 @@
 module.exports = class Recipe {
-    private id: number;
-    private name: string;
     constructor(id) {
         this.id = Number(id);
         this.name = null;
     }
+
     async hydrate() {
         this.name = `Recipe: #${this.id}`;
     }
     toJSON() {
-        return { id: this.id, name: this.name };
+        return {id : this.id, name: this.name};
     }
 };
